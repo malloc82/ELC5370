75ef72eae4fdee4abceff177680c37e12979d49f
Ritchie Cai <ritchiecai@r2labs.org>
2015-10-24 10:37:03 -0500

    Added files for clojure code

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject elc5370 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [net.mikera/core.matrix "0.42.0"]
+                 [net.mikera/vectorz     "0.51.0"]
+                 [net.mikera/vectorz-clj "0.36.0"]
+                 [clatrix "0.5.0"]
+                 [incanter/incanter-charts "1.5.6"]
+                 [incanter/incanter-core   "1.5.6"]])
