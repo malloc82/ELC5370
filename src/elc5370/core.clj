f25c218931b733fc38573a6766d042f4aa1cf0e0
Ritchie Cai <ritchiecai@r2labs.org>
2015-10-26 08:10:36 -0500

    added table data for problem3

--- src/elc5370/core.clj
+++ src/elc5370/core.clj
@@ -1,6 +1,6 @@
 (ns elc5370.core
   (:use clojure.core
-        clj-plot.patch)
+        elc5370.patch)
   (:require [clojure.java.io :as io]
             [incanter.core   :as incanter]
             [incanter.charts :as charts]
@@ -12,4 +12,35 @@
            [mikera.vectorz Vector]))
 
 (set! *warn-on-reflection* true)
+
+(def table {"SPC" 40000
+            "E"	  21912
+            "T"	  16587
+            "A"	  14810
+            "O"	  14003
+            "I"	  13318
+            "N"	  12666
+            "S"	  11450
+            "R"	  10977
+            "H"	  10795
+            "D"	  7874
+            "L"	  7253
+            "U"	  5246
+            "C"	  4943
+            "M"	  4761
+            "F"	  4200
+            "Y"	  3853
+            "W"	  3819
+            "G"	  3693
+            "P"	  3316
+            "B"	  2715
+            "V"	  2019
+            "K"	  1257
+            "X"	  315
+            "Q"	  205
+            "J"	  188
+            "Z"	  128})
+
+(def y (mapv second (sort-by second > table)))
+(def x (mapv first  (sort-by second > table)))
 
