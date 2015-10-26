6358584cc4a19c81e1bd0240abc538f0790f7800
Ritchie Cai <ritchiecai@r2labs.org>
2015-10-26 07:04:50 -0500

    added core library dependencies

--- src/elc5370/core.clj
+++ src/elc5370/core.clj
@@ -1,3 +1,15 @@
-(ns elc5370.core)
+(ns elc5370.core
+  (:use clojure.core
+        clj-plot.patch)
+  (:require [clojure.java.io :as io]
+            [incanter.core   :as incanter]
+            [incanter.charts :as charts]
+            [incanter.stats  :as stats]
+            [clojure.core.matrix :as mat]
+            )
+  (:import [mikera.matrixx Matrix]
+           [mikera.matrixx.impl StridedMatrix]
+           [mikera.vectorz Vector]))
 
+(set! *warn-on-reflection* true)
 
