1727104ffe91e08c62289a8b4de6dfff9ef53259
Ritchie Cai <ritchiecai@r2labs.org>
2015-11-11 11:26:08 -0600

    huffman code for D-ary code and custom code alphabets

--- src/elc5370/huffman.clj
+++ src/elc5370/huffman.clj
@@ -16,40 +16,53 @@
                [k (/ v total)])
              (sort-by second < (persistent! m)))))))
 
+(defn make-dummies
+  [n]
+  (for [i (range n)]
+    [(str/join ["_dummy" (.toString i) "_"]) (int 0)]))
+
+(defn padding
+  "Calculate number of merges, and number of padding to sorted symbol table"
+  [n D]
+  (let [k (long (Math/ceil (/ (dec n) (dec D))))]
+    {:merges k :pad (- (inc (* k (dec D))) n)}))
 
 (defn make-tree
-  [lst]
-  (loop [s   lst
-         len (count lst)]
-    (if (> len 1)
-      (let [t (take 2 s)
-            v (apply + (map second t))
-            sub-lst (drop 2 s)]
-        ;; (println s)
-        ;; (println one)
-        ;; (println two)
-        ;; (println v)
-        ;; (println sub-lst)
-        ;; (println "=====================================")
-        (recur (concat (filter #(< (second %) v) sub-lst)
-                       `(~[t v])
-                       (filter #(>= (second %) v) sub-lst))
-               (dec len)))
-      (first s))))
+  [lst & {:keys [D] :or {D 2}}]
+  (let [{:keys [merges pad]} (padding (count lst) D)]
+    (loop [s (concat (make-dummies pad) lst)
+           k merges]
+      (if (> k 0)
+        (let [[h _s] (split-at D s)
+              v      (apply + (map second h))]
+          (recur (concat (filter #(<  (second %) v) _s)
+                         `(~[h v])
+                         (filter #(>= (second %) v) _s))
+                 (dec k)))
+        (first s)))))
 
 (defn gen-code
-  [tree]
-  (letfn [(f [t s m!]
-            ;; (println t s)
-            (if (coll? (first t))
-              (let [_m (f (first  (first t)) (str/join [s "0"]) m!)]
-                (recur (second (first t)) (str/join [s "1"]) _m))
-              (assoc! m! (first t) s)))]
-    (persistent! (f tree "" (transient {})))))
+  [tree & {:keys [code] :or {code ["0" "1"]}}]
+  (let [D-1 (dec (count code))]
+    (letfn [(walk [t s m!]
+              ;; (println t s)
+              (let [branches (first t)]
+                (if (coll? branches)
+                  (let [m (loop [n  0
+                                 _m m!]
+                            (if (< n D-1)
+                              (recur (inc 0) (walk (nth branches n) (str/join [s (nth code n)]) _m))
+                              _m))]
+                    (recur (last branches) (str/join [s (last code)]) m))
+                  (assoc! m! branches s))))]
+      (persistent! (walk tree "" (transient {}))))))
 
 (defn huffman-code
-  [s]
-  (gen-code (make-tree (elem-count s))))
+  [s & {:keys [code] :or {code ["0" "1"]}}]
+  (let [D (count code)]
+    (-> (elem-count s)
+        (make-tree :D D)
+        (gen-code :code code))))
 
 ;; Test
 (huffman-code "DAVID CAMERON SETS OUT HIS FOUR EU REFORM GOALS  BUT THE EUROPEAN COMMISSION WARNS HIS BENEFIT RESTRICTIONS MAY BE ILLEGAL.")
